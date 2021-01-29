ef3977a9319d7c5f0920eeac818146d2bebf0ecc
Brian McPhillips <B.McPhillips89@gmail.com>
2021-01-28 19:15:42 -0800

    displays exchange rate and calculates from api, enables swap button function

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ function calculate() {
   const currencyOne = currencyElOne.value;
   const currencyTwo = currencyElTwo.value;
 
-  
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.rates[currencyTwo];
+      rateEl.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+      amountElTwo.value = (amountElOne.value * rate).toFixed(2);
+    });
 }
 
 // Event Listeners
@@ -21,4 +27,10 @@ currencyElTwo.addEventListener('change', calculate);
 amountElOne.addEventListener('input', calculate);
 amountElTwo.addEventListener('input', calculate);
 
+swap.addEventListener('click', () => {
+  const temp = currencyElOne.value;
+  currencyElOne.value = currencyElTwo.value;
+  currencyElTwo.value = temp;
+  calculate();
+})
 calculate();
